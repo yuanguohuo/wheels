da4b880a1f0277413cdbc751ea5ea7f583824049
huoyuanguo <huoyuanguo@jd.com>
2019-08-09 16:06:31 +0800

    slab

--- slab/slab.go
+++ slab/slab.go
@@ -8,8 +8,8 @@ import (
 type Deallocator func(*[]byte)
 
 type Slab struct {
-	size     int
-	inflight int64
+	size      int
+	inflights int64
 
 	pool    sync.Pool
 	dealloc Deallocator
@@ -17,28 +17,28 @@ type Slab struct {
 
 func NewSlab(size int) *Slab {
 	s := &Slab{
-		size:     size,
-		inflight: 0,
+		size:      size,
+		inflights: 0,
 	}
 	s.pool = sync.Pool{
 		New: func() interface{} {
-			atomic.AddInt64(&s.inflight, 1)
 			buf := make([]byte, s.size)
 			return &buf
 		},
 	}
 	s.dealloc = func(buf *[]byte) {
-		*buf = (*buf)[0:0]
+		*buf = (*buf)[0:size]
 		s.pool.Put(buf)
-		atomic.AddInt64(&s.inflight, -1)
+		atomic.AddInt64(&s.inflights, -1)
 	}
 	return s
 }
 
 func (s *Slab) Allocate() (*[]byte, Deallocator) {
+	atomic.AddInt64(&s.inflights, 1)
 	return s.pool.Get().(*[]byte), s.dealloc
 }
 
-func (s *Slab) Inflight() int64 {
-	return atomic.LoadInt64(&s.inflight)
+func (s *Slab) Inflights() int64 {
+	return atomic.LoadInt64(&s.inflights)
 }
--- slab/slab_test.go
+++ slab/slab_test.go
@@ -10,7 +10,7 @@ func Test_allocate(t *testing.T) {
 	var s *Slab = NewSlab(1024 * 1024 * 4)
 
 	defer func() {
-		assert.Equal(t, int64(0), s.Inflight())
+		assert.Equal(t, int64(0), s.Inflights())
 	}()
 
 	buf1, dealloc1 := s.Allocate()
@@ -18,22 +18,20 @@ func Test_allocate(t *testing.T) {
 
 	assert.Equal(t, 1024*1024*4, len(*buf1))
 	assert.Equal(t, 1024*1024*4, cap(*buf1))
-	assert.Equal(t, int64(1), s.Inflight())
+	assert.Equal(t, int64(1), s.Inflights())
 
 	buf2, dealloc2 := s.Allocate()
 	defer dealloc2(buf2)
 
 	assert.Equal(t, 1024*1024*4, len(*buf2))
 	assert.Equal(t, 1024*1024*4, cap(*buf2))
-	assert.Equal(t, int64(2), s.Inflight())
+	assert.Equal(t, int64(2), s.Inflights())
 
 	buf3, dealloc3 := s.Allocate()
 	assert.Equal(t, 1024*1024*4, len(*buf3))
 	assert.Equal(t, 1024*1024*4, cap(*buf3))
-	assert.Equal(t, int64(3), s.Inflight())
+	assert.Equal(t, int64(3), s.Inflights())
 
 	dealloc3(buf3)
-	assert.Equal(t, int64(2), s.Inflight())
-	assert.Equal(t, 0, len(*buf3))
-	assert.Equal(t, 1024*1024*4, cap(*buf3))
+	assert.Equal(t, int64(2), s.Inflights())
 }
