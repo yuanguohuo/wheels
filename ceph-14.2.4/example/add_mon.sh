d53ca4f0bb7ef3d0f6361bf3fa9b127f6e081f5c
huoyuanguo <huoyuanguo@jd.com>
2019-11-14 15:23:56 +0800

    multi monitors

---
+++ ceph-14.2.4/example/add_mon.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+docker container run                                    \
+    -d                                                  \
+    -i                                                  \
+    -t                                                  \
+    --privileged=true                                   \
+    --network=ceph-public                               \
+    --name=mon.b                                        \
+    --ip=172.20.0.11                                    \
+    -v $(pwd)/var/lib/ceph:/var/lib/ceph                \
+    -v $(pwd)/var/run/ceph:/var/run/ceph                \
+    -v $(pwd)/etc/ceph:/etc/ceph                        \
+    -e CLUSTER_NAME=clusterfoo                          \
+    -e MON_ID=b                                         \
+    ceph-14.2.4:v1                                      \
+    ceph_add_mon
+
+
+docker container run                                    \
+    -d                                                  \
+    -i                                                  \
+    -t                                                  \
+    --privileged=true                                   \
+    --network=ceph-public                               \
+    --name=mon.c                                        \
+    --ip=172.20.0.12                                    \
+    -v $(pwd)/var/lib/ceph:/var/lib/ceph                \
+    -v $(pwd)/var/run/ceph:/var/run/ceph                \
+    -v $(pwd)/etc/ceph:/etc/ceph                        \
+    -e CLUSTER_NAME=clusterfoo                          \
+    -e MON_ID=c                                         \
+    ceph-14.2.4:v1                                      \
+    ceph_add_mon
